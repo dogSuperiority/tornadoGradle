b37af693dea8a9115bef4a2a836668242870dd36
Dog Superiority <dog.superiority@gmail.com>
2020-01-26 16:52:19 -0600

    Initial Commit

---
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -0,0 +1,13 @@
+package com.example.demo.view
+
+import com.example.demo.app.Styles
+import tornadofx.*
+
+class MainView : View("Hello TornadoFX") {
+    override val root = hbox {
+        label(title) {
+            addClass(Styles.heading)
+            my error2
+        }
+    }
+}
